e52c0a3283b9d0cade70598a5713186e71cb2734
antoinenguyen-09 <61876488+antoinenguyen-09@users.noreply.github.com>
2020-08-30 23:50:27 +0700

    Update Utility.java

--- J1.L.P0021/Utility.java
+++ J1.L.P0021/Utility.java
@@ -1,16 +1,5 @@
-
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author ASUS
- */
 public class Utility {
     public static boolean GetYesNo(String msg)
     {
