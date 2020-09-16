7dfba53782dc49bcf0ef7851d79fab21a0bb50cb
Nguyễn Cao Huy Hoàng <61876488+antoinenguyen-09@users.noreply.github.com>
2020-09-17 00:18:47 +0700

    Create Utility.java

---
+++ J1.S.P0006/Utility.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Utility {
+    public static int getInt(String msg){
+        int i = 0;
+        Scanner sc = new Scanner(System.in);
+        boolean loop = true;
+        do
+        {
+            System.out.println(msg);
+            try
+            {
+                i = Integer.parseInt(sc.nextLine());
+                if(i>0)
+                   loop = false;
+            }
+            catch(NumberFormatException e)
+            {
+                System.out.println(e);
+                sc.nextLine();
+            }
+        }while(loop);
+        System.out.println();
+        return i;
+    }
+    
+    public static int getRandomInRange(int max, int min){
+        if (min >= max) {
+		throw new IllegalArgumentException("max must be greater than min");
+	}
+	Random r = new Random();
+	return r.nextInt((max - min) + 1) + min;
+    }
+}
