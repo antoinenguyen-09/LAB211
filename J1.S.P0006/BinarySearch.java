4a9cd1322c30350c0e14f3573de7152f1f91eefb
Nguyễn Cao Huy Hoàng <61876488+antoinenguyen-09@users.noreply.github.com>
2020-09-17 00:18:02 +0700

    Create  BinarySearch.java

---
+++ J1.S.P0006/BinarySearch.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+/**
+ *
+ * @author ASUS
+ */
+public class BinarySearch {
+    // Returns index of x if it is present in arr[l.. 
+    // r], else return -1 
+    int binarySearch(int arr[], int x) 
+    { 
+        int l = 0, r = arr.length - 1; 
+        while (l <= r) { 
+            int m = l + (r - l) / 2; 
+  
+            // Check if x is present at mid 
+            if (arr[m] == x) 
+                return m; 
+  
+            // If x greater, ignore left half 
+            if (arr[m] < x) 
+                l = m + 1; 
+  
+            // If x is smaller, ignore right half 
+            else
+                r = m - 1; 
+        } 
+  
+        // if we reach here, then element was not present 
+        return -1; 
+    }  
+    
+    void sort(int arr[]) //insertion sort
+    { 
+        int n = arr.length; 
+        for (int i = 1; i < n; ++i) { 
+            int key = arr[i]; 
+            int j = i - 1; 
+  
+            /* Move elements of arr[0..i-1], that are 
+               greater than key, to one position ahead 
+               of their current position */
+            while (j >= 0 && arr[j] > key) { 
+                arr[j + 1] = arr[j]; 
+                j = j - 1; 
+            } 
+            arr[j + 1] = key; 
+        } 
+    }
+    
+    public static void main(String[] args){
+         BinarySearch ob = new BinarySearch();
+         int arr_size, searchValue;
+         arr_size= Utility.getInt("Enter number of array: ");
+         searchValue = Utility.getInt("Enter search value: ");
+         int arr[] = new int[arr_size];
+         for(int i=0; i<arr.length; i++){
+             arr[i] = Utility.getRandomInRange(30, 10);
+         }
+         ob.sort(arr);
+         System.out.println("Sorted array: "+Arrays.toString(arr));
+         System.out.println("Found "+searchValue+" at index: "+ob.binarySearch(arr, searchValue));
+    }
+}
