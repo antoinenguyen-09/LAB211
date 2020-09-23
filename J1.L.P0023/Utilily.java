74b9e8110c9086716cfb5c822134e93dd6c64e1d
Nguyễn Cao Huy Hoàng <61876488+antoinenguyen-09@users.noreply.github.com>
2020-09-23 10:04:11 +0700

    Update Utilily.java

--- J1.L.P0023/Utilily.java
+++ J1.L.P0023/Utilily.java
@@ -1,13 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import java.util.Scanner;
-/**
- *
- * @author ASUS
- */
+
 public class Utilily {
     public static boolean GetYesNo(String msg)
     {
