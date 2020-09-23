6edcd737103b42ed50ee78cbd9d126d28b4b3ca6
Nguyễn Cao Huy Hoàng <61876488+antoinenguyen-09@users.noreply.github.com>
2020-09-23 10:05:27 +0700

    Update MnFruitShop.java

--- J1.L.P0023/MnFruitShop.java
+++ J1.L.P0023/MnFruitShop.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import java.util.Scanner;
 
-/**
- *
- * @author ASUS
- */
 public class MnFruitShop {
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
