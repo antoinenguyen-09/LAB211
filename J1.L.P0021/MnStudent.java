8f929fb2ccb028b14df305eade858359620f5f67
Nguyễn Cao Huy Hoàng <61876488+antoinenguyen-09@users.noreply.github.com>
2020-09-28 14:17:33 +0700

    Update MnStudent.java

--- J1.L.P0021/MnStudent.java
+++ J1.L.P0021/MnStudent.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+
 public class MnStudent {
     static int id=0, semester;
     static String name, course;
-    static StudentList st_list = new StudentList();
+    static Manager manage = new Manager();
+    static ArrayList<Student> st_list = new ArrayList<>();
     
     public static void CreateNewStudent(){
         boolean loop=true;
@@ -22,24 +25,26 @@ public class MnStudent {
     }
     
     public static void Find_and_Sort(){
+        Student st = new Student(id, name, semester, course);
         System.out.println("Find a student by name: ");
         String input_name = Utility.GetString("Enter the name: ", false);
-        st_list.findbyName(input_name); System.out.println();
+        manage.findbyName(input_name, st_list); 
+        System.out.println();
         System.out.println("Sort student list: ");
-        st_list.sort();
-        st_list.print();
+        manage.sort(st_list, st);
+        manage.print(st_list);
     }
     
     public static void Update_or_Delete(){
         int inputID = Utility.getInt("Enter student needed to update or delete's id: "), validID;
-        if((validID=st_list.findbyID(inputID))!=0){
+        if((validID=manage.findbyID(inputID, st_list))!=0){
             do{
                 String choice = Utility.GetString("(Do you want to update (U) or delete (D) student?", false);
                 if(choice.equalsIgnoreCase("U")) {
-                    st_list.update(validID);
+                    manage.update(validID, st_list);
                     break;
                 } else if(choice.equalsIgnoreCase("D")){
-                    st_list.delete(validID);
+                    manage.delete(validID, st_list);
                     break;
                 } else System.out.println("Wrong input!!! Enter again!");
             } while(true);
@@ -47,6 +52,6 @@ public class MnStudent {
     }
     
     public static void report_st(){
-        st_list.report();
+        manage.report(st_list);
     } 
 }
