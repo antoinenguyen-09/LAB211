7092410e45f7324ddba9e57207ba740459275cc9
Nguyễn Cao Huy Hoàng <61876488+antoinenguyen-09@users.noreply.github.com>
2020-09-28 14:16:00 +0700

    Update Student.java

--- J1.L.P0021/Student.java
+++ J1.L.P0021/Student.java
@@ -4,9 +4,8 @@ import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
-import java.util.TreeSet;
 
-public class Student {
+public class Student implements Comparator<Student>{
     private int ID;
     private String name;
     private int semester;
@@ -45,18 +44,21 @@ public class Student {
     public void print(){
         System.out.println(ID+" | "+name+" | "+semester+" | "+course);
     }
-}
-class StudentList extends ArrayList<Student>{
-    public StudentList() {
-        super();
-    }    
-    public int createStudent(Student st){
-        this.add(st);
-        return this.size();
+
+    @Override
+    public int compare(Student st1, Student st2) {
+        return st1.getName().compareTo(st2.getName());
     }
-    public void findbyName(String name){
+}
+class Manager {
+
+    public int createStudent(Student st, ArrayList<Student> st_list){
+        st_list.add(st);
+        return st_list.size();
+    }
+    public void findbyName(String name, ArrayList<Student> st_list){
         boolean exist=false;
-        for(Student st: this){
+        for(Student st: st_list){
             if(st.getName().equals(name)||st.getName().contains(name)){
                 st.print();
                 exist=true;
@@ -64,22 +66,17 @@ class StudentList extends ArrayList<Student>{
         }
         if(exist==false) System.out.print("Student does not exist!");
     }
-    public void sort(){
-        Collections.sort(this, new Comparator<Student>(){
-            @Override
-            public int compare(Student st1, Student st2) {
-                return st1.getName().compareTo(st2.getName());
-            }
-        });
+    public void sort(ArrayList<Student> st_list, Student st){
+        Collections.sort(st_list, st);
     }
-    public void print(){
-        for(Student st: this){
+    public void print(ArrayList<Student> st_list){
+        for(Student st: st_list){
             System.out.println(st.getName()+" | "+st.getSemester()+" | "+st.getCourse());
         }
         System.out.println();
     } 
-    public int findbyID(int id){
-        for(Student st: this){
+    public int findbyID(int id, ArrayList<Student> st_list){
+        for(Student st: st_list){
             if(st.getID()==id){
                 st.print();
                 return id;
@@ -87,10 +84,10 @@ class StudentList extends ArrayList<Student>{
         }
         return 0;
     }
-    public void update(int id){
+    public void update(int id, ArrayList<Student> st_list){
        String name, course;
        int semester;
-       for(Student st:this){
+       for(Student st:st_list){
            if(st.getID()==id){
                name = Utility.GetString("Update student's name: ", false);
                st.setName(name);
@@ -101,16 +98,16 @@ class StudentList extends ArrayList<Student>{
            }
        }
     }
-    public void delete(int id){
-        Iterator itr = this.iterator();
+    public void delete(int id, ArrayList<Student> st_list){
+        Iterator itr = st_list.iterator();
         while(itr.hasNext()){
             Student st= (Student) itr.next();
             if(st.getID()==id) itr.remove();
         }
     }
-    public void report(){
+    public void report(ArrayList<Student> st_list){
         ArrayList<String> st_strlist = new ArrayList<>();
-        for(Student st: this){
+        for(Student st: st_list){
             st_strlist.add(st.getName()+"|"+st.getCourse());
         }
         Set<String> st_strset = new HashSet<>(st_strlist);
@@ -125,4 +122,3 @@ class StudentList extends ArrayList<Student>{
         }
     }
 }
-
