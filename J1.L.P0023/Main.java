c2cf87150ceaf5b830ec4416f7b20072a652d3e7
Nguyễn Cao Huy Hoàng <61876488+antoinenguyen-09@users.noreply.github.com>
2020-09-30 13:05:04 +0700

    Create Main.java

---
+++ J1.L.P0023/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main (String[] args){
+        Menu mn = new Menu();
+        mn.add("1. Create fruit");
+        mn.add("2. View orders");
+        mn.add("3. Shopping (for buyer)");
+        mn.add("4. Exit");
+        int userChoice;
+        do{
+            mn.print();
+            userChoice = mn.getUserChoices();
+            switch(userChoice){
+                case 1:
+                    MnFruitShop.createProduct();
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    System.exit(0);
+                    break;
+            }
+        } while(true);
+    }
+}
