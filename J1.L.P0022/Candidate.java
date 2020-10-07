bac5ae252dccbcefebb25d8a04a275b7a2a76298
Nguyễn Cao Huy Hoàng <61876488+antoinenguyen-09@users.noreply.github.com>
2020-10-07 10:46:55 +0700

    Create Candidate.java

---
+++ J1.L.P0022/Candidate.java
@@ -0,0 +1,81 @@
+public class Candidate {
+    private int id;
+    private String firstname, lastname, address, phone, email;
+    private int birthDate;
+    private int typeCandidate;
+
+    public Candidate(int id, String phone, String firstname, String lastname, String address, int birthDate, int typeCandidate, String email) {
+        this.id = id;
+        this.phone = phone;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.address = address;
+        this.birthDate = birthDate;
+        this.typeCandidate = typeCandidate;
+        this.email = email;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getBirthDate() {
+        return birthDate;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setBirthDate(int birthDate) {
+        this.birthDate = birthDate;
+    }
+    
+    public int getTypeCandidate() {
+        return typeCandidate;
+    }
+
+    public void setTypeCandidate(int typeCandidate) {
+        this.typeCandidate = typeCandidate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
