2f874431521a902fef9f734b50cc3c2c42890a4c
Nguyễn Cao Huy Hoàng <61876488+antoinenguyen-09@users.noreply.github.com>
2020-10-07 18:09:57 +0700

    Create Menu.java

---
+++ J1.L.P0022/Menu.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Menu extends ArrayList<String>{
+    public Menu() {
+        super();
+    }
+
+    void print(){
+        for(String s : this){
+            System.out.println(s);
+        }
+    }
+     
+    static int getInt(String msg){
+        int val=0;
+        boolean check=false;
+        Scanner sc = new Scanner(System.in);
+        do{
+            System.out.println(msg);
+            try{
+              val = Integer.parseInt(sc.nextLine());
+              if((val>=1)&&(val<=4)){
+                    check = true;
+              }
+              else{
+                  System.out.println("Wrong Input!!");
+              }
+            } catch(NumberFormatException e){
+                System.out.println("Wrong Input!!");
+            }
+        } while(check==false); 
+        return val;
+     }
+    int getUserChoices(){
+        return getInt("Please enter 1,2,3 and 4: ");
+    }
+}
