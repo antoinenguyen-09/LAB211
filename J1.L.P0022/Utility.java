b6cb93a0ae44ae4c1346b96de7661b4df6da7f4f
Nguyễn Cao Huy Hoàng <61876488+antoinenguyen-09@users.noreply.github.com>
2020-10-07 18:13:06 +0700

    Update Utility.java

--- J1.L.P0022/Utility.java
+++ J1.L.P0022/Utility.java
@@ -2,12 +2,85 @@ import java.util.Scanner;
 import java.util.regex.Pattern; 
 
 public class Utility {
-    public static int getDate(String msg){
+    public static boolean GetYesNo(String msg)
+    {
+        String s = "";
+        Scanner sc = new Scanner(System.in);
+        boolean loop = true;
+        do
+        {
+            System.out.print(msg);
+            try
+            {
+                s = sc.next("[yn]");
+                loop = false;
+            }
+            catch(Exception e)
+            {
+                System.out.println(e);
+                sc.nextLine();
+            }
+        }while(loop);
+        if(s.charAt(0) == 'y') return true;
+        else return false;
+    }
+    
+    public static String GetString(String msg, boolean isEmpty)
+    {
+        String s = "";
+        Scanner sc = new Scanner(System.in);
+        boolean loop = true;
+        do
+        {
+            System.out.print(msg);
+            try
+            {
+                s = sc.nextLine();
+                if(isEmpty)
+                    loop = false;
+                else if(!s.isEmpty())
+                    loop = false;
+            }
+            catch(Exception e)
+            {
+                System.out.println(e);
+                sc.nextLine();
+            }
+        }while(loop);
+        System.out.println();
+        return s;
+    }
+    
+    public static int getInt(String msg){
         int i = 0;
         Scanner sc = new Scanner(System.in);
         boolean loop = true;
         do
         {
+            System.out.print(msg);
+            try
+            {
+                i = Integer.parseInt(sc.nextLine());
+                if(i<=0){
+                    System.out.println("Invalid input!");
+                    loop = true;
+                } else loop = false;
+            }
+            catch(NumberFormatException e)
+            {
+                System.out.println("Need number format!");
+                sc.nextLine();
+            }
+        }while(loop);
+        System.out.println();
+        return i;
+    }
+    
+    public static int getDate(String msg){
+        int i = 0;
+        Scanner sc = new Scanner(System.in);
+        boolean loop = true;
+        do{
             System.out.println(msg);
             try
             {
@@ -29,8 +102,7 @@ public class Utility {
         String phone;
         Scanner sc = new Scanner(System.in);
         boolean loop = true;
-        do
-        {
+        do{
             boolean isNumber=true;
             int count=0;
             System.out.println(msg);
@@ -64,8 +136,7 @@ public class Utility {
         String email;
         Scanner sc = new Scanner(System.in);
         boolean loop = true;
-        do
-        {
+        do{
             System.out.println(msg);
             email = sc.nextLine();
             if(email == null){
@@ -77,7 +148,72 @@ public class Utility {
             else
                 System.out.println("Invalid email!");
         } while(loop);
+        System.out.println();
         return email;
     }
     
+    public static int getYear(String msg){
+        int i = 0;
+        Scanner sc = new Scanner(System.in);
+        boolean loop = true;
+        do{
+            System.out.println(msg);
+            try
+            {
+                i = Integer.parseInt(sc.nextLine());
+                if(i>=0 && i<=100)
+                   loop = false;
+            }
+            catch(NumberFormatException e)
+            {
+                System.out.println("Invalid number of year!");
+                sc.nextLine();
+            }
+        }while(loop);
+        System.out.println();
+        return i;
+    }
+    
+    public static String getRank(String msg){
+        String rank;
+        Scanner sc = new Scanner(System.in);
+        String rank_type = "[Ee]xcellence|[Gg]ood|[Ff]air|[Pp]oor";
+        boolean loop = true;
+        do{
+            System.out.println(msg);
+            rank = sc.nextLine();
+            if(rank == null){
+                System.out.println("Invalid input!");
+                continue;
+            } 
+            if(rank.matches(rank_type))
+                loop=false;
+            else
+                System.out.println("Invalid rank!");
+        } while(loop);
+        System.out.println();
+        return rank;
+    }
+    
+    public static int getCandidateType(String msg){
+        int i = 0;
+        Scanner sc = new Scanner(System.in);
+        boolean loop = true;
+        do{
+            System.out.println(msg);
+            try
+            {
+                i = Integer.parseInt(sc.nextLine());
+                if(i>=0 && i<=4)
+                   loop = false;
+            }
+            catch(NumberFormatException e)
+            {
+                System.out.println("Invalid type!");
+                sc.nextLine();
+            }
+        }while(loop);
+        System.out.println();
+        return i;
+    }  
 }
